230baf9778fffda5dc6c21027b185b1efe2cf884
Alex Running <runniale000@gmail.com>
2022-07-29 21:28:44 -0500

    completed backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,7 @@ const { AuthenticationError } = require('apollo-server-express');
 const { Parent, Child, } = require('../models');
 const { signToken } = require('../utils/auth');
 
+
 const resolvers = {
 	Query: {
 		parents: async (parent, args, context, info) => {
