2ef24ecb228151a9dd4908f868818ec0db579ee8
micah_z <micah.a.zahn@gmail.com>
2022-08-01 17:11:48 -0500

    add ChoreCard and update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
+// import providers for global state (if nessessary)
+
+// import components
+
+// import pages
+import LoginParent from './pages/loginParent';
+import LoginChild from './pages/loginChild';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+    <Router>
+      <div>
+        
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/login-parent" component={LoginParent} />
+            <Route exact path="/login-child" component={LoginChild} />
+          </Switch>
+        
+      </div>
+    </Router>
+  </ApolloProvider>
   );
 }
 
--- client/src/components/ChoreCard/index.js
+++ client/src/components/ChoreCard/index.js
@@ -1,8 +1,12 @@
-import {Link} from 'react-router-dom';
-
-
 const ChoreCard = (props) => {
+    const {name, description} = props;
 
+    return (
+        <div>
+            <h4>{name}</h4>
+            <p>{description}</p>
+        </div>
+    )
 }
 
-export default ChoreCard
+export default ChoreCard;
