acb029d4bf022328f9edcf7debd23129f2378465
bennettem <emily.pamelabennett@gmail.com>
2022-08-03 19:46:24 -0500

    stylings of dashboard

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,13 +1,25 @@
 import { Link } from 'react-router-dom';
+import Header from "../components/Header"
 import "../App.css";
 
 const Dashboard = () => {
     return(
-        <>
-            <Link to='/login-parent'>Parent Login</Link>
-            <Link to='/login-child'>Child Login</Link>
-            <Link to='/sign-up'>Sign Up</Link>
-        </>
+        <div className="container">
+        <Header />
+        <div className="btnGroup">
+            <Link className="link"to='/login-parent'>
+                <button>Parent Login</button>
+            </Link>
+
+            <Link className="link"to='/login-child'>
+            <button> Child Login</button>
+            </Link>
+
+            <Link className="link"to='/sign-up'>
+            <button>Sign up</button>
+            </Link>
+            </div>
+        </div>
     );
 }
 
